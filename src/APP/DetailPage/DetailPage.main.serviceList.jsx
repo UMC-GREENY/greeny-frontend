b864215dc8e8e85bfb9ae8935df8fa7a7f0ed976
Hana Park <ydjxlst@gmail.com>
2023-07-31 15:32:35 +0900

    feat: 제품 상세페이지 만들기

---
+++ src/APP/DetailPage/DetailPage.main.serviceList.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState, useRef } from "react";
+import * as itemS from "./Styled/DetailPage.main.serviceList.styles";
+
+function serviceList() {
+	return (
+		<>
+			<itemS.serviceListWrapper>
+				<itemS.ListWrapper>
+					<itemS.serviceItem>제품 정보</itemS.serviceItem>
+					<itemS.serviceItem>리뷰</itemS.serviceItem>
+					<itemS.serviceItem>문의</itemS.serviceItem>
+				</itemS.ListWrapper>
+			</itemS.serviceListWrapper>
+		</>
+	);
+}
+
+export default serviceList;
