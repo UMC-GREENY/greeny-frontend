48a617034643cadce0f4627e0c5451650c426504
sol0503 <jbkljo@naver.com>
2023-08-20 01:18:55 +0900

    feat:비밀번호 변경과 경고문자 생성

--- src/APP/Home/MyPages/Mypage.basicinfo.jsx
+++ src/APP/Home/MyPages/Mypage.basicinfo.jsx
@@ -2,8 +2,11 @@ import React, { useState, useEffect } from "react";
 import * as myInfo from "./Styled/MypageInfo.styles";
 import request from "../../Api/request";
 import { ACCESS_TOKEN } from "../../Api/request";
+import { async } from "q";
+import { useNavigate } from "react-router";
 
 function MypageInfo() {
+  const navigate = useNavigate();
   const [userData, setUserData] = useState({
     type: "",
     email: "",
@@ -11,7 +14,9 @@ function MypageInfo() {
     phone: "",
     birth: "",
   });
-
+  const [before, setBefore] = useState("");
+  const [after1, setAfter1] = useState("");
+  const [after2, setAfter2] = useState("");
   useEffect(() => {
     const fetchUserData = async () => {
       try {
@@ -39,8 +44,52 @@ function MypageInfo() {
     fetchUserData();
   }, []);
 
-  const { type, email, name, phone, birth } = userData;
+  const useChangePw = async () => {
+    if (before === "") {
+      return alert("기존 비밀번호를 입력하지 않았습니다.");
+    }
+    if (after1 === "") {
+      return alert("새로운 비밀번호를 입력하지 않았습니다.");
+    }
+    if (after2 === "") {
+      return alert("새로운 비밀번호을 확인하지 않았습니다.");
+    }
+    if (after1 !== after2) {
+      return alert("비밀번호가 일치하지 않습니다.");
+    }
 
+    try {
+      const response = await request.patch(
+        "api/members", // 엔드포인트
+        {
+          passwordToCheck: before,
+          // 요청 본문 데이터
+          passwordToChange: after1,
+          // 새로운 비밀번호
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${window.localStorage.getItem(
+              ACCESS_TOKEN
+            )}`,
+          },
+        }
+      );
+
+      console.log("비밀번호 변경 성공", response);
+      // 성공 처리 로직 추가
+      navigate("/");
+    } catch (error) {
+      console.error("비밀번호 변경 실패", error);
+      // 실패 처리 로직 추가
+      alert(
+        "기존 비밀번호가 잘못되었거나 새로운 비밀번호가 조건에 맞지 않습니다.(조건: 특수문자 1개 이상, 영문+숫자, 15자 이내) "
+      );
+    }
+  };
+
+  const { type, email, name, phone, birth } = userData;
+  console.log(before);
   return (
     <myInfo.Div>
       <myInfo.Wrapper>
@@ -64,15 +113,27 @@ function MypageInfo() {
               </myInfo.Divs>
               <myInfo.Divs>
                 <h3>기존 비밀번호</h3>
-                <input placeholder="특수문자 1개 이상, 영문+숫자, 15자 이내" />
+                <input
+                  placeholder="특수문자 1개 이상, 영문+숫자, 15자 이내"
+                  onChange={(e) => setBefore(e.target.value)}
+                  type="password"
+                />
               </myInfo.Divs>
               <myInfo.Divs>
                 <h3>비밀번호 변경</h3>
-                <input placeholder="특수문자 1개 이상, 영문+숫자, 15자 이내" />
+                <input
+                  placeholder="특수문자 1개 이상, 영문+숫자, 15자 이내"
+                  onChange={(e) => setAfter1(e.target.value)}
+                  type="password"
+                />
               </myInfo.Divs>
               <myInfo.Divs>
                 <h3>비밀번호 확인</h3>
-                <input placeholder="비밀번호 확인" />
+                <input
+                  placeholder="비밀번호 확인"
+                  onChange={(e) => setAfter2(e.target.value)}
+                  type="password"
+                />
               </myInfo.Divs>
               <myInfo.Divs>
                 <h3>이름</h3>
@@ -104,7 +165,12 @@ function MypageInfo() {
             </myInfo.Main>
             <myInfo.Btns>
               <button style={{ backgroundColor: "#CCCCCC" }}>취소하기</button>
-              <button style={{ backgroundColor: "#649D17" }}>변경하기</button>
+              <button
+                style={{ backgroundColor: "#649D17" }}
+                onClick={useChangePw}
+              >
+                변경하기
+              </button>
             </myInfo.Btns>
           </>
         )}
