6af31627ab630f06ecef122dff1b8d6658155e15
Hana Park <ydjxlst@gmail.com>
2023-08-21 18:12:32 +0900

    feat/63 상세페이지 리뷰 작성, 출력, 상세 정보 출력 등...

---
+++ src/APP/DetailPage/DetailPage.main.serviceContent.isViewing.jsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from "react";
+import * as itemS from "./Styled/DetailPage.main.serviceContent.styles"
+import * as tokens from "../tokens";
+import StarRatings from "react-star-ratings";
+import ImageSlider from "./DetailPage.main.serviceContent.slider"
+import request from "../Api/request";
+
+
+function IsViewing(props) {
+
+    const [review, setReview] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                console.log('아이디:', props.data[0].id);
+                if (props.isProduct === true) {
+                    const response = await request.get(`/api/reviews?type=product&id=${props.data[0].id}`);
+                    console.log("URL: ", response.data.fileUrls);
+                    setReview(response.data);
+                } else {
+                    const response = await request.get(`/api/reviews?type=store&id=${props.data[0].id}`);
+                    setReview(response.data);
+                }
+            } catch (error) {
+                console.error('데이터 가져오기 실패', error);
+            }
+        };
+        fetchData();
+    }, [props.data]);
+
+
+    const images = [
+        "https://pbs.twimg.com/media/Dw8nkd2VsAElmJB.jpg",
+        "https://mblogthumb-phinf.pstatic.net/MjAxOTAzMDJfMjE1/MDAxNTUxNTAzNDY4MzI4.DTPWkA-650YKC2p2coa7ef-rqlTSR0eOEgLTHyWY9hIg.bMJuG4AiMh0SVoNyS_kC22xFxOM9sFAwmmyWfmeY_lQg.JPEG.tbzjyn/180414_%ED%8A%B8%EC%9C%84%ED%84%B02.jpg?type=w800",
+        "https://pbs.twimg.com/media/DakKZQhVwAEYN40.jpg"
+    ];
+
+
+
+    // 별점 관련 상태
+    const [rating, setRating] = useState(0);
+
+    return (
+        <>
+            <itemS.ReviewWrapper type="top">
+                <itemS.ReviewWrapper type="text">
+                    <itemS.ContentText type="text">리뷰</itemS.ContentText>
+                </itemS.ReviewWrapper>
+            </itemS.ReviewWrapper>
+            <itemS.overline></itemS.overline>
+            <itemS.ReviewWrapper type="title" style={{ display: "flex", justifyContent: "space-between" }}>
+                <itemS.ContentText type="content"><StarRatings
+                    rating={review.star}
+                    starRatedColor="black"
+                    numberOfStars={5}
+                    name='rating'
+                    starDimension="24px"
+                    starSpacing="2px"
+                /></itemS.ContentText>
+                <itemS.ContentText type="sub" style={{ display: "flex", width: "780px", whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis" }} >{review.content}</itemS.ContentText>
+                <itemS.ContentText type="body1">{review.writerEmail}</itemS.ContentText>
+                <itemS.ContentText type="body1" style={{ color: tokens.colors.grey_40 }}>{review.createdAt}</itemS.ContentText>
+            </itemS.ReviewWrapper>
+            <itemS.ReviewWrapper type="body" style={{ display: "flex", justifyContent: "space-evenly" }}>
+                <itemS.ReviewWrapper type="img">
+                    { review.fileUrls ? (<ImageSlider images={review.fileUrls} />) : (<ImageSlider images={['store_basic.png']} />)}
+                    
+                </itemS.ReviewWrapper>
+                <itemS.ReviewWrapper type="viewContent" style={{ display: "flex", flexDirection: "column", alignItems: "start" }}>
+                    <itemS.ContentText type="sub" style={{ marginTop: "40px", marginLeft: "10px" }}>{review.writerEmail}</itemS.ContentText>
+                    <div style={{ display: "flex", Width: "224px", Height: "44.8px", margin: "10px 0 0 10px"}}>
+                    <StarRatings
+                    rating={review.star}
+                    starRatedColor="black"
+                    numberOfStars={5}
+                    name='rating'
+                    starDimension="40px"
+                    starSpacing="7px"
+                />
+                    </div>
+                    <itemS.ContentText style={{height: "278px", width: "100%", textAlign: "start", margin: "40.2px 0 0 10px"}}>{review.content}</itemS.ContentText>
+                </itemS.ReviewWrapper>
+            </itemS.ReviewWrapper>
+            <itemS.overline></itemS.overline>
+            <itemS.ReviewWrapper type="bottom">
+                <itemS.Btn type="list" onClick={() => props.setIsViewing(false)}>리뷰 목록</itemS.Btn>
+            </itemS.ReviewWrapper>
+        </>
+    )
+}
+
+export default IsViewing;
