4909e906650ad48f2c800787eab3d418688aece3
begojara99 <pch990824@naver.com>
2023-08-21 21:30:41 +0900

    hotfix: 소셜 소스 추가

--- src/APP/Login/Recoil/Recoil.auth.state.js
+++ src/APP/Login/Recoil/Recoil.auth.state.js
@@ -5,6 +5,12 @@ export const isSuccessState = atom({
   default: false, 
 });
 
+export const socialTypeState = atom({
+  key: 'socialTypeState',
+  default: '', 
+});
+
+
 // export const isAutoState = atom({
 // 	key: 'isAutoState',
 // 	default: false, 
